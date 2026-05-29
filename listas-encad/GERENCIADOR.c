2d1eec960c92239f2a0b570f31cc95d560e2f8d0
Gustavo Leão Mota <gustamt2019@gmail.com>
2026-05-29 14:01:46 -0300

    Add linked list node creation function

---
+++ listas-encad/GERENCIADOR.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct NoSimples {
+    int valor;
+    struct NoSimples *prox;
+} NoSimples;
+
+/* Criar nó */
+NoSimples* criarNo(int valor) {
+    NoSimples *novo = (NoSimples*) malloc(sizeof(NoSimples));
+
+    if (novo == NULL) {
+        printf("Erro de alocacao!\n");
+        exit(1);
+    }
+
+    novo->valor = valor;
+    novo->prox = NULL;
+
+    return novo;
+}
